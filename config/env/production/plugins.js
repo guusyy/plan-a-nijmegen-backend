d13bdfb56cd673141690e664bc01a745c2d8a17a
guusyy <guus.noij@gmail.com>
2021-08-15 23:24:04 +0200

    fixed type in config file

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -2,7 +2,7 @@ module.exports = {
   graphql: {
     endpoint: "graphql",
     shadowCRUD: true,
-    playgroundAlways: grue,
+    playgroundAlways: true,
     depthLimit: 8,
     amountLimit: 100,
     apolloServer: {
