2c6fb12998f4bfe2e859b5bd9a72c0ea3e255339
guusyy <guus.noij@gmail.com>
2021-10-09 19:59:18 +0200

    added cloudinary

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,4 +1,4 @@
-module.exports = {
+module.exports = ({ env }) => ({
   graphql: {
     endpoint: "/graphql",
     shadowCRUD: true,
@@ -8,5 +8,13 @@ module.exports = {
     apolloServer: {
       tracing: false
     }
-  }
-}
+  },
+  upload: {
+    provider: 'cloudinary',
+    providerOptions: {
+      cloud_name: env('CLOUDINARY_NAME'),
+      api_key: env('CLOUDINARY_KEY'),
+      api_secret: env('CLOUDINARY_SECRET'),
+    },
+  },
+})
